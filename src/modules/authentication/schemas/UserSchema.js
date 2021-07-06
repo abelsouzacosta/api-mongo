f839c0bdb0f03e4d4ad99be4a29523d9741cd38a
Abel Souza <abelsouzacosta@gmail.com>
2021-07-06 16:52:20 -0300

    feat(authentication): adding user schema and user model
    this allowas to connect to the database and create documents following a pattern

---
+++ src/modules/authentication/schemas/UserSchema.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+/**
+ * Define os campos que teremos dentro de um documento
+ * no mongo
+ */
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    require: true,
+    select: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = UserSchema;
