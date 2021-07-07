d362092755d5a5f9d54b993530f293dd4eafb4f9
Abel Souza <abelsouzacosta@gmail.com>
2021-07-07 13:21:19 -0300

    feat(authentication): loggin user in to the application
    Compare the hashed password with the password passed through the requisition to verify if the user
    has typed the correct password, if the password is incorrect then throw an error

--- src/modules/authentication/controllers/AuthController.js
+++ src/modules/authentication/controllers/AuthController.js
@@ -1,12 +1,30 @@
-const CreateUSerService = require("../services/CreateUserService");
+const CreateUserService = require("../services/CreateUserService");
+const AuthenticateUserService = require("../services/AuthenticateUserService");
 
 exports.register = async (req, res) => {
   try {
     const { name, email, password } = req.body;
 
-    const user = await CreateUSerService.execute({ name, email, password });
+    const user = await CreateUserService.execute({ name, email, password });
 
     return res.status(200).json({ user });
+  } catch (error) {
+    return res.status(400).json({
+      error: `${error}`,
+    });
+  }
+};
+
+exports.authenticate = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const authenticatedUser = await AuthenticateUserService.execute({
+      email,
+      password,
+    });
+
+    return res.status(200).json(authenticatedUser);
   } catch (error) {
     return res.status(400).json({
       error: `${error}`,
--- src/modules/authentication/routes/index.js
+++ src/modules/authentication/routes/index.js
@@ -3,4 +3,6 @@ const controller = require("../controllers/AuthController");
 
 router.post("/register", controller.register);
 
+router.post("/authenticate", controller.authenticate);
+
 module.exports = router;
